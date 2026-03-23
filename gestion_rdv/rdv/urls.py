b93413da195b3c684b5aa6afbab67b548be3fd84
Wassa <wassa170714@gmail.com>
2026-03-23 20:43:13 +0000

    Ajout requirements.txt

--- gestion_rdv/rdv/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.accueil, name='accueil'),
-    path('rendezvous/', views.ajouter_rdv, name='ajouter_rdv'),
-    path('liste/', views.liste_rdv, name='liste_rdv'),
-    path('supprimer/<int:id>/', views.supprimer_rdv, name='supprimer_rdv'),
-]
